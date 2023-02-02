a7b014d4d0d49a08f92fab9001a1daf70dfa9ec5
yashdaga03 <yashu.yd.03@gmail.com>
2023-02-02 17:13:04 +0530

    calc testing junit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+
+//    static double add(double... operands) {
+//        return DoubleStream.of(operands)
+//                .sum();
+//    }
+    static int add(int a, int b) {
+        return a+b;
+    }
+
+//    static double multiply(double... operands) {
+//        return DoubleStream.of(operands)
+//                .reduce(1, (a, b) -> a * b);
+//    }
+}
